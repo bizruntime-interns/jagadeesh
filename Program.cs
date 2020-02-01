2b2c2fb67517759eac55d0426ba25d7d5305b8f3
jagadeeshgo <47549598+jagadeeshgo@users.noreply.github.com>
2020-02-01 11:07:18 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace switching
+{
+    class switchcases
+    {
+        static void Main(string[] args)
+        {
+            int day = 4;
+            switch (day)
+            {
+                case 5:
+                    Console.WriteLine("today is sat");
+                    break;
+                case 6:
+                    Console.WriteLine("today is sun");
+                    break;
+                case 4:
+                    Console.WriteLine("you are fool");
+                    break;
+                default:
+                    Console.WriteLine("oh yes");
+                    break;
+            }
+        }
+    }
+}
