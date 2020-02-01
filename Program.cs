d6777a2917ecc12e7ce02f202d19b25e31deea05
jagadeeshgo <47549598+jagadeeshgo@users.noreply.github.com>
2020-02-01 10:48:20 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace access_modifiers
+{
+    public class person
+    {
+        private DateTime _birthday;
+        public void SetBirthday(DateTime birthday)
+        {
+            _birthday = birthday;
+        }
+        public DateTime GetBirthday()
+        {
+            return _birthday;
+        }
+    }
+    class bday { 
+        static void Main(string[] args)
+        {
+            var person = new person();
+            person.SetBirthday(new DateTime(1998, 2, 2));
+            Console.WriteLine(person.GetBirthday());
+        }
+    }
+}
