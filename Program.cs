1d5e89f50d7c4cc040458fd0375d4eb46724e383
jagadeeshgo <47549598+jagadeeshgo@users.noreply.github.com>
2020-02-01 11:03:23 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace exceptionthrow
+{
+    class exceptiothrow
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                a();
+                b();
+                c(null);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+        static void a()
+        {
+            try
+            {
+                int value = 1;
+                int.Parse("0");
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        static void b()
+        {
+            try
+            {
+                int value = 1;
+                int.Parse("0");
+            }
+            catch (DivideByZeroException ex)
+            {
+                throw ex;
+            }
+        }
+                    static void c(string value)
+                    {
+                        if(value == null){
+                            throw new ArgumentNullException("null");
+
+
+                        }
+                    }
+                }
+            }
+        
+    
+
