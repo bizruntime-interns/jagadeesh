312236ff7ccfff0b85a526b651918c680fd97909
jagadeeshgo <47549598+jagadeeshgo@users.noreply.github.com>
2020-02-01 10:54:36 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+    class exceptionhandling
+    {
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] numbers = { 1, 2, 3, 4 };
+                Console.WriteLine(numbers[20]);
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(Exception )
+            {
+                Console.WriteLine("PLEASE ENTER THE INDEX IN GIVEN RANGE");
+            }
+
+            finally
+            {
+                Console.WriteLine("the try catch is finished");
+            }
+        }
+    }
+
+
