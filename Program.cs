aa47073cdcde993afd2cdff5cf9f449d2fa5c952
jagadeeshgo <47549598+jagadeeshgo@users.noreply.github.com>
2020-02-01 10:44:40 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ABSTRACTION
+{
+    abstract class Animal
+
+    {
+        public abstract void animalsound();
+        public void sleep()
+        {
+            Console.WriteLine("zzzz");
+        }
+    }
+    class Pig : Animal
+    {
+        public override void animalsound()
+        {
+            Console.WriteLine("weeeeeeeeeee");
+        }
+    }
+    class abstraction { 
+
+    static void Main(string[] args)
+        {
+            Pig mypig = new Pig();
+            mypig.animalsound();
+            mypig.sleep();
+        }
+    }
+}
